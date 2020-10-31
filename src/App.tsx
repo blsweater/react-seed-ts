584716ad3a6edd5f51bb06d9628d8c7040a2b810
blsweater <blsweater@gmail.com>
2020-10-31 14:14:13 +0700

    First blood

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          react-seed-ts
         </a>
       </header>
     </div>
