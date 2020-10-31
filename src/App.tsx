864404aba4faa1086e182239786450ac3dc0d5b6
blsweater <blsweater@gmail.com>
2020-10-31 14:21:02 +0700

    Test commit

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          react-seed-ts
+          react-seed-ts edited
         </a>
       </header>
     </div>
